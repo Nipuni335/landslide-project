fd5064f7d41ec4b214ba839f80503963a730717d
Nipuni355 <kavindya.20010511@gmail.com>
2026-01-13 19:58:31 +0530

    next.js
    update new one

--- frontend/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-blue-600 text-white p-4 text-center mt-10">
-      © 2026 Landslide Awareness Blog. All rights reserved.
-    </footer>
-  );
-}
